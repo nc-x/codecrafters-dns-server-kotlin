4720f431f325a5fc0b4e7412b69de518cd45e829
Neelesh Chandola <neelesh.chandola@outlook.com>
2024-10-31 09:51:13 +0000

    ser/de the message format (header, question, answer)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,15 +3,16 @@ import io.ktor.network.sockets.*
 import io.ktor.utils.io.core.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
-import kotlin.io.use
 
 fun main(args: Array<String>) = runBlocking {
     val selectorManager = SelectorManager(Dispatchers.IO)
     aSocket(selectorManager).udp().bind(InetSocketAddress("127.0.0.1", 2053)).use { socket ->
         while (true) {
             val datagram = socket.receive()
+            val incoming = Message.from(datagram.packet)
             val packet = buildPacket {
-                write(Header.default().toByteArray())
+                val outgoing = Message.responseFor(incoming)
+                write(outgoing.toByteArray())
             }
             socket.send(Datagram(packet, datagram.address))
         }
--- src/main/kotlin/Message.kt
+++ src/main/kotlin/Message.kt
@@ -1,11 +1,14 @@
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
+import kotlinx.io.Buffer
+import kotlinx.io.Source
+import kotlinx.io.readByteArray
+import kotlinx.io.readString
+import kotlinx.io.writeString
 
-typealias Byte3 = Short
+typealias Bit3 = Byte
 
 data class Header(
     val id: Short,
-    // +Short
+    // +Flags: Short
     val qr: Boolean,
     val opcode: Byte,
     val aa: Boolean,
@@ -13,53 +16,220 @@ data class Header(
     val rd: Boolean,
     val ra: Boolean,
     val z: Byte,
-    val rcode: Byte3,
-    // -Short
+    val rcode: Bit3,
+    // -Flags: Short
     val qdcount: Short,
     val ancount: Short,
     val nscount: Short,
     val arcount: Short,
 ) {
 
-    fun toByteArray(): ByteArray {
-        val buffer = ByteBuffer.allocate(12).order(ByteOrder.BIG_ENDIAN)
-        buffer.putShort(id)
-        val packed = ((if (qr) 1 else 0) shl 15 or
-                (opcode.toInt() and 0b1111 shl 11) or
-                (if (aa) 1 else 0 shl 10) or
-                (if (tc) 1 else 0 shl 9) or
-                (if (rd) 1 else 0 shl 8) or
-                (if (ra) 1 else 0 shl 7) or
-                (z.toInt() and 0b1111 shl 3) or
+    fun toByteArray(buffer: Buffer) {
+        buffer.writeShort(id)
+        val packed = (((if (qr) 1 else 0) shl 15) or
+                ((opcode.toInt() and 0b1111) shl 11) or
+                ((if (aa) 1 else 0) shl 10) or
+                ((if (tc) 1 else 0) shl 9) or
+                ((if (rd) 1 else 0) shl 8) or
+                ((if (ra) 1 else 0) shl 7) or
+                ((z.toInt() and 0b1111) shl 3) or
                 (rcode.toInt() and 0b111)).toShort()
-        buffer.putShort(packed.toShort())
-        buffer.putShort(qdcount)
-        buffer.putShort(ancount)
-        buffer.putShort(nscount)
-        buffer.putShort(arcount)
-        return buffer.array()
+        buffer.writeShort(packed.toShort())
+        buffer.writeShort(qdcount)
+        buffer.writeShort(ancount)
+        buffer.writeShort(nscount)
+        buffer.writeShort(arcount)
     }
 
     companion object {
-        fun Int.toBoolean() = this.toInt() == 1
+        fun from(source: Source): Header {
+            val id = source.readShort()
+            val packed = source.readShort().toInt()
+            val qr = (packed shr 15) and 1 == 1
+            val opcode = ((packed shr 11) and 0b1111).toByte()
+            val aa = (packed shr 10) and 1 == 1
+            val tc = (packed shr 9) and 1 == 1
+            val rd = (packed shr 8) and 1 == 1
+            val ra = (packed shr 7) and 1 == 1
+            val z = ((packed shr 3) and 0b1111).toByte()
+            val rcode = (packed and 0b111).toByte()
+            val qdcount = source.readShort()
+            val ancount = source.readShort()
+            val nscount = source.readShort()
+            val arcount = source.readShort()
 
-        fun default(): Header {
             return Header(
-                id = 1234,
-                qr = 1.toBoolean(),
-                opcode = 0,
-                aa = 0.toBoolean(),
-                tc = 0.toBoolean(),
-                rd = 0.toBoolean(),
-                ra = 0.toBoolean(),
-                z = 0,
-                rcode = 0,
-                qdcount = 0,
-                ancount = 0,
-                nscount = 0,
-                arcount = 0
+                id,
+                qr,
+                opcode,
+                aa,
+                tc,
+                rd,
+                ra,
+                z,
+                rcode,
+                qdcount,
+                ancount,
+                nscount,
+                arcount,
             )
         }
     }
 
+}
+
+// https://www.rfc-editor.org/rfc/rfc1035#section-3.2.2
+enum class Type(val value: Short) {
+    A(1),
+    NS(2),
+    MD(3),
+    MF(4),
+    CNAME(5),
+    SOA(6),
+    MB(7),
+    MG(8),
+    MR(9),
+    NULL(10),
+    WKS(11),
+    PTR(12),
+    HINFO(13),
+    MINFO(14),
+    MX(15),
+    TXT(16);
+
+    companion object {
+        fun from(value: Short): Type =
+            Type.entries.first { it.value == value }
+    }
+}
+
+// https://www.rfc-editor.org/rfc/rfc1035#section-3.2.4
+enum class Class(val value: Short) {
+    IN(1),
+    CS(2),
+    CH(3),
+    HS(4);
+
+    companion object {
+        fun from(value: Short): Class =
+            Class.entries.first { it.value == value }
+    }
+}
+
+data class Question(
+    val domainName: String,
+    val type: Type,
+    val clazz: Class,
+) {
+    @OptIn(ExperimentalStdlibApi::class)
+    fun toByteArray(buffer: Buffer) {
+        buffer.writeDomainName(domainName)
+        buffer.writeShort(type.value)
+        buffer.writeShort(clazz.value)
+    }
+
+    companion object {
+        fun from(source: Source) =
+            Question(
+                domainName = parseDomainName(source),
+                type = Type.from(source.readShort()),
+                clazz = Class.from(source.readShort())
+            )
+    }
+}
+
+data class Answer(
+    val domainName: String,
+    val type: Type,
+    val clazz: Class,
+    val ttl: Int,
+    val rdata: String,
+) {
+    fun toByteArray(buffer: Buffer) {
+        buffer.writeDomainName(domainName)
+        buffer.writeShort(type.value)
+        buffer.writeShort(clazz.value)
+        buffer.writeInt(ttl)
+        when (type) {
+            Type.A -> {
+                buffer.writeShort(4)
+                val ipParts = rdata.split(".")
+                for (part in ipParts) buffer.writeByte(part.toByte())
+            }
+
+            else -> error("RDATA is only implemented for A currently.")
+        }
+
+    }
+}
+
+data class Message(
+    val header: Header,
+    val question: Question,
+    val answer: Answer?,
+) {
+    fun toByteArray(): ByteArray {
+        val buffer = Buffer()
+        header.toByteArray(buffer)
+        question.toByteArray(buffer)
+        answer?.toByteArray(buffer)
+        return buffer.readByteArray()
+    }
+
+    companion object {
+        fun responseFor(incoming: Message): Message =
+            Message(
+                header = Header(
+                    id = incoming.header.id,
+                    qr = true, // 1
+                    opcode = incoming.header.opcode,
+                    aa = false, // 0
+                    tc = false, // 0
+                    rd = incoming.header.rd,
+                    ra = false, // 0
+                    z = 0,
+                    rcode = if (incoming.header.opcode.toInt() == 0) 0 else 4,
+                    qdcount = 1,
+                    ancount = 1,
+                    nscount = 0,
+                    arcount = 0,
+                ),
+                question = incoming.question,
+                answer = Answer(
+                    domainName = incoming.question.domainName,
+                    type = incoming.question.type,
+                    clazz = incoming.question.clazz,
+                    ttl = 60,
+                    rdata = "8.8.8.8"
+                )
+            )
+
+        fun from(source: Source): Message {
+            val header = Header.from(source)
+            val question = Question.from(source)
+            return Message(header, question, null)
+        }
+    }
+}
+
+private fun parseDomainName(source: Source): String {
+    var payloadLen: Byte
+    val name = StringBuilder()
+    do {
+        payloadLen = source.readByte()
+        name.append(source.readString(payloadLen.toLong()))
+        name.append(".")
+    } while (payloadLen != 0.toByte())
+    return name.toString()
+}
+
+private fun Buffer.writeDomainName(domainName: String) {
+    domainName
+        .split("\\W+".toRegex())
+        .filter { it.isNotEmpty() }
+        .forEach {
+            writeByte(it.length.toByte())
+            writeString(it)
+        }
+    writeByte(0)
 }
